dcf0416082c421a6186f0e6edbe724840ed87d99
pipo411 <ariel.gonzales@fundacion-jala.org>
2018-08-12 16:47:42 -0400

    spring-boot-web-support
    spring-boot-web-support  project

---
+++ src/main/java/com/alibaba/boot/web/BootApplication.java
@@ -0,0 +1,21 @@
+package com.alibaba.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Application
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see SpringBootApplication
+ * @since 2017.03.29
+ */
+@EnableAutoConfiguration
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
